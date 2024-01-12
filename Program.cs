5513058be039156b18290267e1e44cb6f623f5c8
sebl <sebl@schnellebuntebilder.de>
2024-01-12 18:24:10 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+
+
+using (PowerShell ps = PowerShell.Create())
+{
+    var pid = PowerShellHelpers.Run("test");
+
+    Console.WriteLine(pid);
+
+}
+
+
+public class PowerShellHelpers
+{
+    public static int? Run(string input)
+    {
+        Collection<PSObject> ps_output;
+
+        using (PowerShell ps = PowerShell.Create())
+        {
+            ps.AddScript(@"$process = start-process C:\vvvv\vvvv_gamma_5.3-0432-gebaa53fdbe\vvvv.exe --allowmultiple -passthru; $process.id");
+
+            ps_output = ps.Invoke();
+        }
+
+        return Int32.Parse(ps_output.FirstOrDefault().ToString()); ;
+    }
+}
